9382df177d61ff918494e379a95b509bc05dee53
Martin <martindealmeida17@gmail.com>
2025-05-20 13:58:34 -0300

    First Commit

---
+++ src/componentes/Modal/index.tsx
@@ -0,0 +1,54 @@
+import React, { forwardRef,  useRef } from "react";
+import {
+  ButtonGroup,
+  CloseButton,
+  ModalContainer,
+  ModalHeader,
+  ModalOverlay,
+} from "./style";
+import Botao from "../Botao";
+
+interface ModalProps {
+  icon: React.ReactNode;
+  titulo: string;
+  children: React.ReactNode;
+  aoClicar: () => void;
+}
+
+export interface ModalHandle {
+  open: () => void;
+  close: () => void;
+}
+const Modal = forwardRef<ModalHandle ,ModalProps>(({
+  icon,
+  titulo,
+  children,
+  aoClicar,
+}, ref) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  return (
+    <ModalOverlay>
+      <ModalContainer ref={dialogRef}>
+        <ModalHeader>
+          <div>
+            {icon}
+            {titulo}
+          </div>
+          <CloseButton>x</CloseButton>
+        </ModalHeader>
+        {children}
+        <ButtonGroup>
+          <Botao $variante="secundario">
+            Cancelar
+          </Botao>
+          <Botao $variante="primario" onClick={aoClicar}>
+            Adicionar
+          </Botao>
+        </ButtonGroup>
+      </ModalContainer>
+    </ModalOverlay>
+  );
+});
+
+export default Modal;
